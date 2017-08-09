9b9c137e76e6824cf6a21d2c0aebe59fae9a5df5
Jeremy Clerico <blops2hd@gmail.com>
2017-08-09 17:58:48 +0100

    Building GoalsVC UI and Outlets/Actions

---
+++ GoalPost/Controller/GoalsVC.swift
@@ -0,0 +1,36 @@
+//
+//  GoalsVC.swift
+//  GoalPost
+//
+//  Created by Jeremy Clerico on 09/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //Variables
+    
+    //Constants
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        print("A")
+    }
+    
+    
+    
+}
+
