b85e8bee298122aaa74988d3393c852912dfea24
Jeremy Clerico <blops2hd@gmail.com>
2017-08-09 22:55:52 +0100

    Saved Goal Data to Persistent Store

--- GoalPost/Controller/FinishGoalVC.swift
+++ GoalPost/Controller/FinishGoalVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
     
@@ -32,8 +33,40 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func createGoalBtnWasPressed(_ sender: Any) {
-        //Pass data into Core Data Goal Model
+        if pointsTextField.text != "" {
+        self.save { (complete) in
+            if complete {
+                dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+           try managedContext.save()
+            print("Successfully Saved Data")
+            completion(true)
+        } catch {
+            debugPrint("Could not save: \(error)")
+            completion(false)
+        }
+    }
+    
+    
+    
     
     
     
