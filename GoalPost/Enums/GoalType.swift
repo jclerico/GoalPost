5bd44cb660f6e2b6ef081e34911975bf38f344bc
Jeremy Clerico <blops2hd@gmail.com>
2017-08-09 19:32:51 +0100

    Displayed Static data in GoalCells and created Enum for GoalType

---
+++ GoalPost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Jeremy Clerico on 09/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
--- GoalPost/View/GoalCell.swift
+++ GoalPost/View/GoalCell.swift
@@ -21,9 +21,9 @@ class GoalCell: UITableViewCell {
     //Constants
     
     
-    func configureCell(description : String, type: String, goalProgressAmount: Int) {
+    func configureCell(description : String, type: GoalType, goalProgressAmount: Int) {
         self.goalDescriptionLbl.text = description
-        self.goalTypeLbl.text = type
+        self.goalTypeLbl.text = type.rawValue
         self.goalProgressLbl.text = String(describing: goalProgressAmount)
         
     }
