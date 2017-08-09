b4f37449143ecf3e806bc1ba1b6013ba2356f8e2
Jeremy Clerico <blops2hd@gmail.com>
2017-08-09 21:27:22 +0100

    Created CreateGoalVC UI and VC and connected Outlets&Actions

---
+++ GoalPost/Controller/CreateGoalVC.swift
@@ -0,0 +1,43 @@
+//
+//  CreateGoalVC.swift
+//  GoalPost
+//
+//  Created by Jeremy Clerico on 09/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+}
